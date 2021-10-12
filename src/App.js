47ccc635c1486e48d6fd00d368d99b9c4d052e29
Yisehak Ebrahim <yisehakebrahim02@gmail.com>
2021-10-12 12:50:58 -0400

    Adding stylesheet links to material libraries, and 1)Adding sample data to app and 2)including materials import + usage in index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { Component } from "react"
 
+class App extends Component{
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div> </div>
-  );
+    this.state = {
+      notes: [
+        {
+          id: "5c83c052-60da-425f-a302-9d4735a9d6ae",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut.",
+          text: "In orci urna, feugiat sed est id, sodales pharetra nisl. Etiam mollis vehicula imperdiet. In iaculis convallis leo, a pretium erat rutrum in. Ut lacus sem, lobortis id lacinia nec, imperdiet at quam. Etiam ut risus ligula. Mauris ac enim ut purus iaculis lobortis. Donec euismod vel risus ac blandit. Sed in odio enim. Aliquam dolor nibh, luctus a fermentum dignissim, ornare nec enim. Sed posuere in arcu eu ultricies.",
+        },
+        {
+          id: "8fd80679-73ff-4425-ae6d-cffe4463d64f",
+          title: "Cras bibendum libero eu viverra tristique.",
+          text: "Sed et elit id ex lobortis dictum sed vel ex. Mauris id massa interdum, maximus nisl sed, dignissim magna. Donec eu leo varius, malesuada mauris fringilla, porta erat. Nullam consequat auctor tincidunt. Mauris eget lacus ex. Aliquam nibh sem, placerat at nunc at, fringilla auctor dolor. Aliquam eros elit, tempor ut erat eget, interdum porta lacus. Suspendisse in bibendum enim.",
+        },
+        {
+          id: "95747b61-c935-45e9-a156-325307bade96",
+          title: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div> 
+        {JSON.stringify(this.state.notes, null, 2)}
+      </div>
+    );
+    }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import {CssBaseline} from "@material-ui/core";
 
 ReactDOM.render(
-    <App />,
+  <>
+    <App />
+    <CssBaseline />
+  </>,
   document.getElementById('root')
 );
 
